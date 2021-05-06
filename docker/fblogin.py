a4ced368353224a926a90fd7e204c54eb4c9de4a
aaaaa <phamhung.bk94@gmail.com>
2021-05-06 14:43:11 +0700

    Update fblogin.py

--- docker/fblogin.py
+++ docker/fblogin.py
@@ -3,7 +3,32 @@ from selenium.webdriver.chrome.service import Service
 browser = webdriver.Chrome(executable_path="chromedriver")
 
 driver = browser.get("https://www.facebook.com")
-print(driver.title)
+
+FB_URL = "https://fb.com"
+
+txtUser = browser.find_element_by_id("email")
+txtUser.send_keys("thangnch") # <---  Điền username thật của các bạn vào đây
+
+txtPass = browser.find_element_by_id("pass")
+txtPass.send_keys("passwordfake")
+
 sleep(5)
 
 browser.close()
+
+
+class FacebookLogin:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.driver = webdriver.Firefox()
+ 
+    def login(self):
+        pass
+ 
+    def verify_login(self):
+        pass
+    
+    
+def random_sleep(min_s, max_s):
+    sleep(randint(min_s, max_s))
